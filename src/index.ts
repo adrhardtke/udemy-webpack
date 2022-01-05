f20dd6e70d30b8f65090bf473dde89c397178f68
Adriano Hardtke <adriano.hardtke@invillia.com>
2022-01-05 14:55:55 -0300

    remove typescript

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import App from './App.tsx'
-
-// ReactDOM.render(<App />, document.querySelector('#root'))
-
-let color: string = "blue"
-console.log(color)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
@@ -22,15 +22,7 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
         ]
-    },
-    resolve: {
-        extensions: ['.tsx','.ts', '.js']
     },
     plugins: [
         new HtmlWebpackPlugin({
