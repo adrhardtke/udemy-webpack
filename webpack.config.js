02597a836a9f9eaadf79778eb9157f644d80e2b0
Adriano Hardtke <adriano.hardtke@invillia.com>
2022-01-05 14:58:09 -0300

    typescript remove react

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,13 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: '/'
-    },
-    mode: 'development',
-    devServer: {
-        historyApiFallback: true
+        filename: 'main.js'
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
@@ -32,10 +18,4 @@ module.exports = {
     resolve: {
         extensions: ['.tsx','.ts', '.js']
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public/index.html'),
-            filename: 'index.html'
-        })
-    ]
 }
